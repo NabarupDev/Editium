e43906d074f250bdefeda2f22c5058bbc8fad880
NabarupDev <roynabarup20@gmail.com>
2025-10-29 02:16:16 +0530

    feat: enable word count display in the Editium editor

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -46,7 +46,7 @@ const App: React.FC = () => {
           onChange={handleChange}
           onImageUpload={handleImageUpload}
           height="200px"
-          showWordCount={false}
+          showWordCount={true}
         />
       </div>
       
